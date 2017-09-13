033e376eb18668b6fe4bf7cf061e604cbd633e6f
kunalgarg2100 <kunalgarg2100@gmail.com>
2017-09-13 18:13:07 +0530

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 #	  validates :name, presence: true
 	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 #he way to do this is with a callback, which is a method that gets invoked at a particular point in the lifecycle of an Active Record object. 
@@ -15,9 +17,39 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 	# Returns the hash digest of the given string.
-  	def User.digest(string)
+  	def self.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     	BCrypt::Password.create(string, cost: cost)
   	end
+
+  	def self.new_token
+    	SecureRandom.urlsafe_base64
+  	end
+  	
+  	def remember
+    	self.remember_token = User.new_token
+    	update_attribute(:remember_digest, User.digest(remember_token))
+  	end
+
+  	def authenticated?(remember_token) #o verify that a given remember token matches the user’s remember digest, 
+    	return false if remember_digest.nil?
+    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  	end
+
+  	# Forgets a user.
+  	def forget
+    	update_attribute(:remember_digest, nil)
+  	end
+
+  	def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,6 +49,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
+
 end
 
 '''
