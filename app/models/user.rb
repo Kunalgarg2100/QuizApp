4cb81ad6853af0b22a29253234d428550b23e09a
kunalgarg2100 <kunalgarg2100@gmail.com>
2017-09-13 23:58:12 +0530

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
 	attr_accessor :remember_token
 
 #	  validates :name, presence: true
@@ -15,7 +16,7 @@ class User < ApplicationRecord
 	validates(:name, presence: true, length: { maximum: 49 })
 	validates(:email, presence: true,length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false })
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, presence: true, length: { minimum: 6 },allow_nil: true
 	# Returns the hash digest of the given string.
   	def self.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
